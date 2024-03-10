8099c86a47f1369dc4daac10d8cd4855942cecb6
Dư Thanh Được <duthanhduoc@gmail.com>
2024-03-10 11:03:44 +0700

    12. Server Component

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,13 +1,23 @@
-'use client'
-import { useState } from 'react'
+// 'use client'
+import { websites } from '@/lib/data'
+// import { useState } from 'react'
 
 export default function LoginPage() {
-  const [email, setEmail] = useState('duthanhduoc@gmail.com')
-  console.log('LoginPage')
+  // const [email, setEmail] = useState('duthanhduoc@gmail.com')
   return (
     <div>
       Login page
-      {email}
+      <ul>
+        {websites.map((item) => {
+          return (
+            <li key={item.url}>
+              <a href={item.url} target='_blank' rel='noreferrer'>
+                {item.name}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
---
+++ client/src/lib/data.ts
@@ -0,0 +1,54 @@
+export const websites = [
+  {
+    name: 'Google',
+    url: 'https://www.google.com',
+    description: 'A web search engine developed by Google.'
+  },
+  {
+    name: 'Facebook',
+    url: 'https://www.facebook.com',
+    description: 'A social networking site.'
+  },
+  {
+    name: 'YouTube',
+    url: 'https://www.youtube.com',
+    description: 'A video sharing platform.'
+  },
+  {
+    name: 'Twitter',
+    url: 'https://www.twitter.com',
+    description: 'A microblogging and social networking service.'
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com',
+    description: 'A professional networking platform.'
+  },
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com',
+    description: 'A photo and video sharing social networking service.'
+  },
+  {
+    name: 'Wikipedia',
+    url: 'https://www.wikipedia.org',
+    description: 'A free online encyclopedia.'
+  },
+  {
+    name: 'Amazon',
+    url: 'https://www.amazon.com',
+    description:
+      'An online marketplace, manufacturer of e-book readers, and Web services provider.'
+  },
+  {
+    name: 'WhatsApp',
+    url: 'https://www.whatsapp.com',
+    description:
+      'A freeware, cross-platform messaging and Voice over IP service.'
+  },
+  {
+    name: 'Tencent QQ',
+    url: 'https://www.qq.com',
+    description: 'An instant messaging software service and web portal.'
+  }
+]
